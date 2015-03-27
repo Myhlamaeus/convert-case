fcb915eed84e65f43e517b85f27f8258ed2e4d47
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-03-27 20:26:16 +0000

    Change indentation to 2 spaces

--- case.js
+++ case.js
@@ -1,117 +1,117 @@
 const types = {
-    dash: require('case-dash'),
-    snake: require('case-snake'),
-    dot: require('case-dot'),
-    camel: require('case-camel')
+  dash: require('case-dash'),
+  snake: require('case-snake'),
+  dot: require('case-dot'),
+  camel: require('case-camel')
 }
 
 const methods = ['parse', 'stringify', 'is']
 
 function concat (a, b) {
-    return a.concat(b)
+  return a.concat(b)
 }
 
 function isMethod (key) {
-    return (key in this) && typeof this[key] === 'function'
+  return (key in this) && typeof this[key] === 'function'
 }
 
 function isType (type) {
-    return types[type].is(this)
+  return types[type].is(this)
 }
 
 export function addCase (name, obj) {
-    name = String(name)
+  name = String(name)
 
-    if(typeof obj !== 'object') {
-        throw new Error('Can only add new cases of type object')
-    }
+  if(typeof obj !== 'object') {
+    throw new Error('Can only add new cases of type object')
+  }
 
-    if(!methods.every(isMethod, obj)) {
-        throw new Error(`Added cases must have the methods ${methods}`)
-    }
+  if(!methods.every(isMethod, obj)) {
+    throw new Error(`Added cases must have the methods ${methods}`)
+  }
 
-    types[name] = obj
+  types[name] = obj
 }
 
 export function identify (val) {
-    const types = this.identifyAll(val)
+  const types = this.identifyAll(val)
 
-    if(types.length) {
-        return types[0]
-    }
-    return undefined
+  if(types.length) {
+    return types[0]
+  }
+  return undefined
 }
 
 export function identifyAll (val) {
-    if(typeof val === 'undefined' || val === null) {
-        return []
-    }
+  if(typeof val === 'undefined' || val === null) {
+    return []
+  }
 
-    val = String(val)
+  val = String(val)
 
-    return Object.keys(types).filter(isType, val)
+  return Object.keys(types).filter(isType, val)
 }
 
 export function parseAs (val, type) {
-    if(!(type in types)) {
-        throw new Error(`Unknown case: ${type}`)
-    }
+  if(!(type in types)) {
+    throw new Error(`Unknown case: ${type}`)
+  }
 
-    return types[type].parse(val)
+  return types[type].parse(val)
 }
 
 export function parse (val) {
-    if(typeof val === 'undefined' || val === null) {
-        return []
-    }
+  if(typeof val === 'undefined' || val === null) {
+    return []
+  }
 
-    val = String(val)
-    const type = this.identify(val)
-    if(typeof type === 'undefined') {
-        return val
-    }
-    return this.parseAs(val, type)
+  val = String(val)
+  const type = this.identify(val)
+  if(typeof type === 'undefined') {
+    return val
+  }
+  return this.parseAs(val, type)
 }
 
 export function parseAll (val) {
-    let ret = []
+  let ret = []
 
-    if(typeof val === 'undefined' || val === null) {
-        return []
-    }
+  if(typeof val === 'undefined' || val === null) {
+    return []
+  }
 
-    ret.push(String(val))
+  ret.push(String(val))
 
-    for(let type of identifyAll(val)) {
-        ret = ret.map(function (str) {
-            return parseAs(str, type)
-        }).reduce(concat, [])
-    }
+  for(let type of identifyAll(val)) {
+    ret = ret.map(function (str) {
+      return parseAs(str, type)
+    }).reduce(concat, [])
+  }
 
-    return ret
+  return ret
 }
 
 export function stringify (val, type) {
-    if(!(type in types)) {
-        throw new Error(`Unknown case: ${type}`)
-    }
+  if(!(type in types)) {
+    throw new Error(`Unknown case: ${type}`)
+  }
 
-    return types[type].stringify(val)
+  return types[type].stringify(val)
 }
 
 export function convert (val, fromType, toType) {
-    let parsed
+  let parsed
 
-    if(arguments.length < 3) {
-        toType = fromType
-        fromType = undefined
-    }
+  if(arguments.length < 3) {
+    toType = fromType
+    fromType = undefined
+  }
 
-    if(typeof fromType === 'undefined') {
-        parsed = this.parseAll(val)
-    } else {
-        parsed = this.parseAs(val, fromType)
-    }
+  if(typeof fromType === 'undefined') {
+    parsed = this.parseAll(val)
+  } else {
+    parsed = this.parseAs(val, fromType)
+  }
 
-    return this.stringify(parsed, toType)
+  return this.stringify(parsed, toType)
 }
